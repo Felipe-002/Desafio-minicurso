4d0ae32e88dbecc8a1f4ef3ffe4703cb09625a5d
Felipe de Oliveira <felipe.deoliveira.rpg@gmail.com>
2022-01-16 22:06:36 -0300

    Implementar funções para conectar com a API, e sortear aleatoriamente 3 personagens

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ traduzirCondicao = (data) => {
 gerarValorAletorio = () => {
     return Math.floor(Math.random() * 671);
 }
+
+pegarPersonagem = () => {
+    for (personagem = 1; personagem <= 3; personagem = personagem + 1) {
+        const imagem = document.querySelector(`#imagem${personagem}`);
+        const nomeDoPersonagem =document.querySelector(`#nome${personagem}`);
+        const especie = document.querySelector(`#especie${personagem}`);
+        const condicao = document.querySelector(`#status${personagem}`);
+    let numeroAleatorio = gerarValorAletorio();
+    fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.species;
+        condicao.innerHTML = traduzirCondicao(data);
+    });
+}
+}
