651011b9feeee0ad6acdeea592680bf1c969bdca
Felipe de Oliveira <felipe.deoliveira.rpg@gmail.com>
2022-01-16 22:03:55 -0300

    Implementar função para interpretar os dados informando sobre o status e mostrar para o usuário

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const botao = document.querySelector('button');
+
+traduzirCondicao = (data) => {
+    if(data.status == 'unknown'){
+        return 'Não sabemos';
+    }else if(data.status == 'Alive'){
+        return 'Sim';
+    }else {
+        return 'Não. Está morto';
+    }
+}
